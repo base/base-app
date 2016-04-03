0145c5264878d5f5fe23465be7649a8e23b47c6b
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-03 13:48:28 -0400

    remove unused utils

--- utils.js
+++ utils.js
@@ -11,18 +11,7 @@ require = utils; // eslint-disable-line
 require('base-generators', 'generators');
 require('base-pipeline', 'pipeline');
 require('base-pkg', 'pkg');
-require('base-runtimes', 'runtimes');
-require('define-property', 'define');
-require('extend-shallow', 'extend');
 require = fn; // eslint-disable-line
-
-/**
- * Return true if `val` is an instance of `app` or the given `ctorName`
- */
-
-utils.isApp = function(val, ctorName) {
-  return utils.isObject(val) && val['is' + ctorName] === true;
-};
 
 /**
  * Expose `utils`
